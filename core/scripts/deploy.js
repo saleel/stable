4e3474b5493ac1dfe1edb15683f2b75ae9b9bf73
Saleel Salam <saleel97@gmail.com>
2022-01-09 09:13:26 +0000

    Update deploy scripts

--- core/scripts/deploy.js
+++ core/scripts/deploy.js
@@ -4,7 +4,10 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const fs = require("fs");
+const path = require("path");
 const ethernal = require("hardhat-ethernal");
+const stableArtifact = require("../artifacts/contracts/Stable.sol/Stable.json");
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -26,6 +29,15 @@ async function main() {
   });
 
   console.log("Stable deployed to:", stable.address);
+
+  console.log("Updating subgraph ABI");
+
+  fs.writeFileSync(
+    path.join(__dirname, "../../subgraph/abis/Stable.json"),
+    JSON.stringify(stableArtifact.abi, null, 2)
+  );
+
+  console.log("Updated subgraph ABI successfully");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
