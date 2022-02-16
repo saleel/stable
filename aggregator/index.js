55f2a6e3b69a15648da2f86e8b829833f4d1848c
Saleel Salam <saleel97@gmail.com>
2022-02-16 23:34:06 +0000

    Improve aggregator code to sleep and ocntinue

--- aggregator/index.js
+++ aggregator/index.js
@@ -1,172 +1,23 @@
-require("dotenv").config();
-const { ethers } = require("ethers");
-const axios = require("axios");
-const stableAbi = require("./abis/Stable.json");
-const szrAbi = require("./abis/StabilizerToken.json");
-const countryTrackerAbi = require("./abis/CountryTracker.json");
-const { assert } = require("chai");
+/* eslint-disable no-console */
+require('dotenv').config();
+const assert = require('assert');
+const { ethers } = require('ethers');
+const axios = require('axios');
+const stableAbi = require('./abis/Stable.json');
+const szrAbi = require('./abis/StabilizerToken.json');
+const countryTrackerAbi = require('./abis/CountryTracker.json');
 
+const SLEEP_INTERVAL = 10 * 1000;
 
-async function claimAggregationRound() {
-
-}
-
-async function beginAggregation() {
-  const account = process.env.ACCOUNT;
-  const provider = new ethers.providers.JsonRpcProvider({
-    url: process.env.RPC_URL
-  });
-  const signer = provider.getSigner(account);
-
-  /** @type {import("./typechain-types/Stable").Stable} */
-  const stableContract = new ethers.Contract(process.env.STABLE_CONTRACT, stableAbi, provider).connect(signer);
-
-  /** @type {import("./typechain-types/SZRToken").SZRToken} */
-  const srzContract = new ethers.Contract(await stableContract.szrToken(), szrAbi, provider).connect(signer);
-
-  let countries = ["UK", "US", "IN"];
-
-  // Prepare country contracts
-  /** @type{Record<string, import("./typechain-types/CountryTracker").CountryTracker >} */
-  let countryContracts = {};
-  for (const country of countries) {
-    const address = await stableContract.countryTrackers(country);
-    const countryContract = new ethers.Contract(address, countryTrackerAbi, provider);
-    countryContracts[country] = countryContract.connect(signer);
-  }
-
-  // Check aggregation status/role
-  if (await stableContract.currentAggregator() !== account) {
-    console.log("Not an aggregator now");
-
-    const lockedAmount = await stableContract.aggregatorLockedAmounts(account);
-    if (lockedAmount === 0) {
-      console.log("Enrolling as aggregator");
-      await srzContract.approve(process.env.STABLE_CONTRACT, 1000);
-      await stableContract.enrollAsAggregator(20);
-    }
-
-    const canClaimAggregationRound = await stableContract.canClaimNextAggregationRound(account);
-
-    if (canClaimAggregationRound) {
-      console.log("Trying to become aggregator");
-      await stableContract.claimNextAggregationRound();
-    } else {
-      console.log("Cannot become aggregator");
-      return;
-    }
-  }
-
-  assert((await stableContract.currentAggregator()).toLowerCase() === account, "Not aggregator");
-
-  const aggregationEndTime = await countryContracts["US"].aggregationRoundEndTime();
-  const currentTime = new Date().getTime() / 1000;
-
-  if (currentTime < aggregationEndTime) {
-    console.log("Aggregation round not due");
-    return;
-  }
-
-  console.log("Starting aggregation");
-
-  for (const [country, contract] of Object.entries(countryContracts)) {
-    await updatePrice(contract);
-  }
-
-  await stableContract.completeAggregation();
-
-  console.log("Aggregation round completed");
-}
-
-/**
- * 
- * @param {import("./typechain-types/CountryTracker").CountryTracker} contract 
- * @returns 
- */
-async function updatePrice(contract) {
-  const currency = await contract.currency();
-  const country = await contract.country();
-  const aggregationRoundId = await contract.aggregationRoundId();
-
-  console.log(`Updating prices for ${country} and date ${new Date(aggregationRoundId * 1000).toISOString()}`);
-
-  const graphAggregationRoundId = country + "-" + currency + "-" + aggregationRoundId;
-
-  const query = `
-    {
-      stable (id: "${country}-${currency}") {
-        productBasket {
-          productId
-          weightage
-        }
-      }
-      prices(where: { country: "${country}" }) {
-        product {
-          id
-        }
-        value
-      }
-      priceSubmissions(where: { aggregationRound: "${graphAggregationRoundId}" }, first: 1000) {
-        country
-        currency
-        price
-        aggregationRound {
-          id
-        }
-        product {
-          id
-          name
-        }
-        stable {
-          productBasket {
-            productId
-            weightage
-          }
-        }
-        source
-        createdBy
-        createdAt
-      }
-    }
-  `;
-
-  const { data } = await axios({
-    url: "http://127.0.0.1:8000/subgraphs/name/stable",
-    method: "POST",
-    data: {
-      query
-    }
-  });
-
-  const { priceSubmissions, stable, latestPrices } = data.data;
-  const { productBasket } = stable;
-
-  const productPrices = calculatePrices(priceSubmissions, aggregationRoundId);
-
-  console.log(aggregationRoundId);
-
-  if (Object.keys(productPrices).length === 0) {
-    console.log("Nothing to update");
-    return;
-  }
-
-  const priceIndex = calculatePriceIndex(productBasket, latestPrices, productPrices);
-
-  await contract.updatePrices(aggregationRoundId, Object.keys(productPrices), Object.values(productPrices), priceIndex);
-
-  console.log("Price and Price Index updated for ", country);
-}
-
-const MIN_CONFIRMATIONS = 1;
-
-// TOP3_AVG
-function calculatePrices(submissions, aggregationRoundId) {
+// Assume that contract priceAggregationMethod is "TOP3_AVG"
+// TODO: Need to explicitly check priceAggregationMethod
+function calculateAveragePrice({ priceSubmissions, minPriceConfirmations }) {
   const productPrices = {};
 
-  const priceOccurrences = {}
+  const priceOccurrences = {};
 
-  for (const submission of submissions) {
-    const { price, product: { id: productId }, currency } = submission;
+  for (const submission of priceSubmissions) {
+    const { price, product: { id: productId } } = submission;
 
     if (!priceOccurrences[productId]) {
       priceOccurrences[productId] = {};
@@ -179,21 +30,23 @@ function calculatePrices(submissions, aggregationRoundId) {
     const prices = priceOccurrences[productId];
 
     const p = Object.entries(prices)
-      .filter(e => e[1] >= MIN_CONFIRMATIONS)
+      .filter((e) => e[1] >= minPriceConfirmations)
       .sort((a, b) => b[1] - a[1]);
 
     if (p.length === 0) {
+      // eslint-disable-next-line no-continue
       continue;
     }
 
     const thirdMostConfirmation = p?.[2]?.[1] ?? p[0][1];
 
     const topPrices = p.filter((x, i) => i < 3 || x[1] >= thirdMostConfirmation)
-      .map(a => Number(a[0]));
+      .map((a) => Number(a[0]));
 
     const average = topPrices.reduce((acc, i) => acc + i, 0) / topPrices.length;
 
     if (average === 0) {
+      // eslint-disable-next-line no-continue
       continue;
     }
 
@@ -203,7 +56,7 @@ function calculatePrices(submissions, aggregationRoundId) {
   return productPrices;
 }
 
-function calculatePriceIndex(productBasket, latestPrices, productPrices) {
+async function calculatePriceIndex({ productBasket, contract, productPrices }) {
   let totalPrice = 0;
   let weightageSum = 0;
 
@@ -212,10 +65,12 @@ function calculatePriceIndex(productBasket, latestPrices, productPrices) {
 
     let price = productPrices[productId];
     if (!price) {
-      price = latestPrices.find(l => l.product.id === productId)?.value ?? 0;
+      // eslint-disable-next-line no-await-in-loop
+      price = await contract.prices(productId);
     }
 
     if (!price) {
+      // eslint-disable-next-line no-continue
       continue;
     }
 
@@ -228,6 +83,164 @@ function calculatePriceIndex(productBasket, latestPrices, productPrices) {
   }
 
   return Math.round(totalPrice / weightageSum);
+}
+
+/**
+ *
+ * @param {{ contract: import("./typechain-types/CountryTracker").CountryTracker}} contract
+ * @returns
+ */
+async function startAggregationForCountry({ contract, minPriceConfirmations }) {
+  const currency = await contract.currency();
+  const country = await contract.country();
+  const aggregationRoundId = await contract.aggregationRoundId();
+  const aggregationRoundEndTime = await contract.aggregationRoundEndTime();
+
+  console.log(`Updating prices for ${country} and date ${new Date(aggregationRoundId * 1000).toISOString()}`);
+
+  const query = `
+    {
+      countryTracker (id: "${country}-${currency}") {
+        productBasket {
+          productId
+          weightage
+        }
+      }
+      priceSubmissions(where: { country: "${country}" createdAt_gte: ${aggregationRoundId}, createdAt_lt: ${aggregationRoundEndTime} }, first: 1000) {
+        country
+        currency
+        price
+        product {
+          id
+          name
+        }
+        source
+        createdBy
+        createdAt
+      }
+    }
+  `;
+
+  const { data } = await axios({
+    url: 'http://127.0.0.1:8000/subgraphs/name/stable',
+    method: 'POST',
+    data: {
+      query,
+    },
+  });
+
+  if (data.errors) {
+    throw new Error(`Error from GraphQL \n${JSON.stringify(data.errors, null, 2)}`);
+  }
+
+  const { priceSubmissions, countryTracker } = data.data;
+  const { productBasket } = countryTracker;
+
+  console.log(`Found ${priceSubmissions.length} submissions for ${country} between ${aggregationRoundId} and ${aggregationRoundEndTime}`);
+  const productPrices = calculateAveragePrice({ priceSubmissions, minPriceConfirmations });
+
+  if (Object.keys(productPrices).length === 0) {
+    console.log('Nothing to update');
+    return false;
+  }
+
+  const priceIndex = await calculatePriceIndex({ productBasket, contract, productPrices });
+
+  console.log(`New Price Index for ${country} is ${priceIndex}`);
+
+  try {
+    await contract.updatePrices(aggregationRoundId, Object.keys(productPrices), Object.values(productPrices), priceIndex);
+  } catch (error) {
+    if (!error.message.includes('Price already updated')) {
+      throw error;
+    }
+  }
+
+  console.log(`Price and Price Index updated for ${country}`);
+
+  return true;
+}
+
+async function beginAggregation() {
+  const account = process.env.ACCOUNT;
+  const provider = new ethers.providers.JsonRpcProvider({
+    url: process.env.RPC_URL,
+  });
+  const signer = provider.getSigner(account);
+
+  /** @type {import("./typechain-types/Stable").Stable} */
+  const stableContract = new ethers.Contract(process.env.STABLE_CONTRACT, stableAbi, provider).connect(signer);
+
+  /** @type {import("./typechain-types/SZRToken").SZRToken} */
+  const srzContract = new ethers.Contract(await stableContract.szrToken(), szrAbi, provider).connect(signer);
+
+  const countries = ['UK', 'US', 'IN'];
+
+  // Prepare country contracts
+  /** @type{Record<string, import("./typechain-types/CountryTracker").CountryTracker >} */
+  const countryContracts = {};
+  for (const country of countries) {
+    // eslint-disable-next-line no-await-in-loop
+    const address = await stableContract.countryTrackers(country);
+    const countryContract = new ethers.Contract(address, countryTrackerAbi, provider);
+    countryContracts[country] = countryContract.connect(signer);
+  }
+
+  // Check aggregation status/role
+  if ((await stableContract.currentAggregator()).toLowerCase() !== account.toLowerCase()) {
+    console.log('Not an aggregator now');
+
+    const lockedAmount = await stableContract.aggregatorLockedAmounts(account);
+    if (lockedAmount === 0) {
+      console.log('Enrolling as aggregator');
+      await srzContract.approve(process.env.STABLE_CONTRACT, 1000);
+      await stableContract.enrollAsAggregator(20);
+    }
+
+    const canClaimAggregationRound = await stableContract.canClaimNextAggregationRound(account);
+
+    if (canClaimAggregationRound) {
+      console.log('Trying to become aggregator');
+      await stableContract.claimNextAggregationRound();
+    } else {
+      console.log('Cannot become aggregator');
+      return;
+    }
+  }
+
+  assert.strictEqual((await stableContract.currentAggregator()).toLowerCase(), account);
+
+  const aggregationEndTime = await countryContracts.US.aggregationRoundEndTime();
+  const currentTime = Math.round(new Date().getTime() / 1000);
+
+  if (currentTime < aggregationEndTime) {
+    console.log('Aggregation round not due');
+    return;
+  }
+
+  console.log('Starting aggregation');
+
+  const minPriceConfirmations = await stableContract.mininumPriceConfirmations();
+
+  let updatedCount = 0;
+  for (const [country, contract] of Object.entries(countryContracts)) {
+    console.log(`Starting aggregation for country ${country}`);
+    // eslint-disable-next-line no-await-in-loop
+    const result = await startAggregationForCountry({ contract, minPriceConfirmations });
+    if (result) {
+      updatedCount += 1;
+    }
+  }
+
+  if (updatedCount) {
+    await stableContract.completeAggregation();
+    console.log('Aggregation round completed');
+  }
+
+  // Sleep and check aggregation again
+  setTimeout(async () => {
+    await beginAggregation();
+  }, SLEEP_INTERVAL);
 }
 
 beginAggregation()
