c7dc9244ff35712db74b1d7c52ea212c2cc2a0e6
Saleel Salam <saleel97@gmail.com>
2022-02-20 20:40:07 +0000

    Add icon for futures

--- ui/src/components/product-image.js
+++ ui/src/components/product-image.js
@@ -3,6 +3,15 @@ import React from 'react';
 function ProductImage(props) {
   const { product, width } = props;
 
+  if (product.category === 'Futures') {
+    return (
+      <div className="futures-icon">
+        <div>{product.id}</div>
+        <div>=F</div>
+      </div>
+    );
+  }
+
   return (
     <img
       src={`/assets/${product.id}.png`}
